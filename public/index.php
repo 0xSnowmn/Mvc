c2e27af4fc56197ea8b13191f4f7a276b2eef8a7
Youssef Ahmed <ya860374@gmail.com>
2018-11-15 22:53:34 +0200

    Create Project Index

---
+++ public/index.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Mvc;
+
+//$
+//var_dump(explode('/',trim($url,'/'),3));
+include '../app/config.php';
+include APP_PATH . 'lib/autoload.php';
+
+use Mvc\Lib\Frontcontroller;
+
+$front = new Frontcontroller();
+$front->dispatch();
+
+
+
+
+
+
+
+?>
+    <h1>Welcome to Mvc World</h1>
+
