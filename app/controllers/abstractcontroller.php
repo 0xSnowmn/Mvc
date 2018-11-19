1ecdd87c57892b3d64306ede1f58f06fe1ff2e4a
Youssef Ahmed <ya860374@gmail.com>
2018-11-19 17:58:17 +0200

    Add Set Functions

--- app/controllers/abstractcontroller.php
+++ app/controllers/abstractcontroller.php
@@ -3,7 +3,37 @@
 namespace Mvc\Controllers;
 
 class AbstractController {
+    
+    protected $_controller;
+    protected $_action;
+    protected $params;
+
     public function notFoundAction() {
-        echo 'Sorry This Page Doesn\'t Exists ';
+        $this->_view();
+    }
+
+    public function setController($controller) {
+        $this->_controller = $controller;
+    }
+
+    public function setAction($action) {
+        $this->_action = $action;
+    }
+
+    public function setParams($param) {
+        $this->params = $param;
+    }
+    
+    protected function _view() {
+
+        $view = VIEW_PATH . $this->_controller . '/' . $this->_action . '.v.php';
+        if($this->_action == 'notFoundAction') {
+            $view = VIEW_PATH . 'notfound/notfound.v.php';
+        }
+        if(file_exists($view)) {
+            require $view;
+        } else {
+            require VIEW_PATH . 'notfound/noview.v.php';
+        }
     }
 }
