a7328e89eea544bf38dc3ab2213fb570f56b0d34
youssuf14 <ya860374@gmail.com>
2018-11-20 13:12:40 +0200

    Change

--- app/config.php
+++ app/config.php
@@ -6,7 +6,7 @@ defined('VIEW_PATH') ? NULL : define('VIEW_PATH', APP_PATH . 'views/');
 
 // Database Credentials
 defined('DATABASE_HOST_NAME')       ? null : define ('DATABASE_HOST_NAME', 'localhost');
-defined('DATABASE_USER_NAME')       ? null : define ('DATABASE_USER_NAME', 'root');
+defined('DATABASE_USER_NAME')       ? null : define ('DATABASE_USER_NAME', 'youssef');
 defined('DATABASE_PASSWORD')        ? null : define ('DATABASE_PASSWORD', 'rebo');
 defined('DATABASE_DB_NAME')         ? null : define ('DATABASE_DB_NAME', 'storedb');
 defined('DATABASE_PORT_NUMBER')     ? null : define ('DATABASE_PORT_NUMBER', 3306);
