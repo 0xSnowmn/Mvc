cf8a087bd2b56539d9ac87ac51fa0c3e89321b88
youssuf14 <ya860374@gmail.com>
2018-11-29 11:32:38 +0200

    Add create View

---
+++ app/views/users/create.v.php
@@ -0,0 +1,46 @@
+<?php ?>
+<form autocomplete="off" class="appForm clearfix" method="post" enctype="application/x-www-form-urlencoded">
+    <fieldset>
+        <legend><?= $text_legend ?></legend>
+        <div class="input_wrapper n20 border">
+            <label><?= $text_label_FirstName ?></label>
+            <input required type="text" name="FirstName" maxlength="10" value="">
+        </div>
+        <div class="input_wrapper n20 border padding">
+            <label><?= $text_label_LastName ?></label>
+            <input required type="text" name="LastName" maxlength="10" value="">
+        </div>
+        <div class="input_wrapper n20 padding border">
+            <label><?= $text_label_Username ?></label>
+            <input required type="text" name="Username" maxlength="30" value="">
+        </div>
+        <div class="input_wrapper n20 border padding">
+            <label><?= $text_label_Password ?></label>
+            <input required type="password" name="Password" value="">
+        </div>
+        <div class="input_wrapper n20 padding">
+            <label><?= $text_label_CPassword ?></label>
+            <input required type="password" name="CPassword" value="">
+        </div>
+        <div class="input_wrapper n30 border">
+            <label><?= $text_label_Email ?></label>
+            <input required type="email" name="Email" maxlength="40" value="">
+        </div>
+        <div class="input_wrapper n30 border padding">
+            <label><?= $text_label_CEmail ?></label>
+            <input required type="email" name="CEmail" maxlength="40" value="">
+        </div>
+        <div class="input_wrapper n20 padding border">
+            <label><?= $text_label_PhoneNumber ?></label>
+            <input required type="text" name="PhoneNumber" value="">
+        </div>
+        <!--close-- >
+        <div class="input_wrapper_other padding n20 select">
+            <select required name="GroupId">
+                <option value=""></option>
+            </select>
+        </div>
+        </!-- clodw -->
+        <input class="no_float" type="submit" name="submit" value="<?= $text_label_save ?>">
+    </fieldset>
+</form>
