5b57c191459eeabd6a03303263783a46e6d5de18
youssuf14 <ya860374@gmail.com>
2018-12-07 18:57:41 +0200

    indexco

--- app/controllers/indexcontroller.php
+++ app/controllers/indexcontroller.php
@@ -4,10 +4,10 @@ namespace Mvc\Controllers;
 
 class IndexController extends AbstractController {
     public function defaultAction() {
-        
-        //$this->language->load('index|default');
+        var_dump($this->messanger);
+        $this->language->load('index|default');
         $this->_view();
-        var_dump($this);
+        
 
     }
 
