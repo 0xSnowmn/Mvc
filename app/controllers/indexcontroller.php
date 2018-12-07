2c097df136d004c61d1541f265c4a234c13581c5
youssuf14 <ya860374@gmail.com>
2018-12-07 02:17:53 +0200

    Views

--- app/controllers/indexcontroller.php
+++ app/controllers/indexcontroller.php
@@ -5,13 +5,14 @@ namespace Mvc\Controllers;
 class IndexController extends AbstractController {
     public function defaultAction() {
         
-        $this->_language->load('index|default');
+        //$this->language->load('index|default');
         $this->_view();
+        var_dump($this);
 
     }
 
     public function createAction() {
-        $this->_language->load('index|default');
+        $this->language->load('index|default');
         $this->_view();
     }
 }
