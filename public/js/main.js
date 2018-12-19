827c70f516f38b78d4310727aeaf61de768ee050
youssuf14 <ya860374@gmail.com>
2018-12-19 14:44:39 +0200

    Add Ajax Request

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,4 @@
-$('table.data').DataTable(
-    {
-        "aaSorting": [],
-        "stateSave": true
-    }
-);
+
 
 $('a.menu_switch').click(function(evt)
 {
@@ -83,4 +78,34 @@ $('li.submenu > a').click(function()
         $(this).parent().addClass('selected')
     }
 });
+
+(function(){
+    var userNameField = document.querySelector('input[name=Username]');
+    if(null !== userNameField) {
+        userNameField.addEventListener('blur',function(){
+            var req = new XMLHttpRequest();
+            req.open('POST','http://www.mvc.org/users/checkUserAjax');
+            req.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+            req.onreadystatechange = function() {
+                var iElem = document.createElement('i');
+                if(req.readyState == req.DONE && req.status == 200) {
+                    if(req.response == 0) {
+                        iElem.className = 'fa fa-times error';
+                    } else if(req.response == 1) {
+                        iElem.className = 'fa fa-check success';
+                    }
+                    var iElems = userNameField.parentNode.childNodes;
+                    for ( var i = 0, ii = iElems.length; i < ii; i++ )
+                    {
+                        if(iElems[i].nodeName.toLowerCase() == 'i') {
+                            iElems[i].parentNode.removeChild(iElems[i]);
+                        }
+                    }
+                    userNameField.parentNode.appendChild(iElem);
+                }
+            }
+            req.send("Username=" + this.value);
+        },false);
+    }
+})();
 
